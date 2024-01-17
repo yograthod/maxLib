0fe4b898750f9c7fc9a1ad51a55bc8cf52d490b9
yograthod <156900119+yograthod@users.noreply.github.com>
2024-01-17 15:22:02 +0530

    first commit.
    package first commit.

---
+++ Sources/MaxLib/MaxLib.swift
@@ -0,0 +1,11 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+
+public struct MaxLib {
+    public init() {}
+    
+    public func helloWorld() {
+        print("Hello, World!")
+    }
+}
